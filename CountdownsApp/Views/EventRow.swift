653f3a775bc62d9c4702b88f8cad385a72fa4cb7
mizech <michael.zech@hotmail.de>
2024-08-15 13:31:00 +0200

    Usage of RelativeDateFormatter

--- CountdownsApp/Views/EventRow.swift
+++ CountdownsApp/Views/EventRow.swift
@@ -2,13 +2,14 @@ import SwiftUI
 
 struct EventRow: View {
     var event: Event
+    let formatter = RelativeDateTimeFormatter()
     
     var body: some View {
         VStack(alignment: .leading) {
             Text(event.title)
                 .font(.headline)
                 .foregroundStyle(event.textColor)
-            Text("\(event.date)")
+            Text(formatter.localizedString(for: event.date, relativeTo: Date.now))
         }
     }
 }
