1b792b230ede8c1a231a562f7b440511425ad800
mizech <michael.zech@hotmail.de>
2024-08-15 10:06:33 +0200

    Initial Commit

---
+++ CountdownsApp/CountdownsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountdownsAppApp.swift
+//  CountdownsApp
+//
+//  Created by Michael on 15.08.24.
+//
+
+import SwiftUI
+
+@main
+struct CountdownsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
