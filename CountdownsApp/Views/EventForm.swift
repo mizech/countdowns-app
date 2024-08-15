efaf76ab8254a67b88d297ba7d8269fba0f012f9
mizech <michael.zech@hotmail.de>
2024-08-15 13:16:16 +0200

    Impl.: onSave()

--- CountdownsApp/Views/EventForm.swift
+++ CountdownsApp/Views/EventForm.swift
@@ -1,9 +1,12 @@
 import SwiftUI
 
 struct EventForm: View {
+    @Environment(\.dismiss) var dismiss
     @Environment(EventsViewModel.self) var eventsVM
+    
     @State var event = Event(title: "", date: .now, textColor: .gray)
-    var index: Int
+    var title: String
+    var index: Int? = nil
     
     var body: some View {
         Form {
@@ -11,27 +14,41 @@ struct EventForm: View {
             DatePicker("Date", selection: $event.date, displayedComponents: [.date, .hourAndMinute])
             ColorPicker("Color", selection: $event.textColor)
         }
-        .navigationTitle("Add Event")
+        .navigationTitle("\(title)")
         .navigationBarTitleDisplayMode(.inline)
         .toolbar {
             ToolbarItem(placement: .topBarTrailing) {
                 Button(action: {
-                    guard event.title.isEmpty == false else {
-                        return
-                    }
-                    
-                    eventsVM.events[index] = event
+                    onSave()
                 }, label: {
-                    Label("Submit", systemImage: "pencil")
+                    Label("Submit", systemImage: "checkmark")
                 })
             }
         }
         .onAppear {
-            self.event = eventsVM.events[index]
+            if let index = index {
+                self.event = eventsVM.events[index]
+            }
         }
+    }
+    
+    func onSave() {
+        guard event.title.isEmpty == false else {
+            return
+        }
+        
+        if title == "Edit" {
+            if let index = index {
+                eventsVM.events[index] = event
+            }
+        } else {
+            eventsVM.appendEvent(event: event)
+        }
+        
+        dismiss()
     }
 }
 
 #Preview {
-    EventForm(event: Event(title: "", date: Date.now, textColor: .black), index: 3)
+    EventForm(title: "Edit")
 }
--- CountdownsApp/Views/EventsView.swift
+++ CountdownsApp/Views/EventsView.swift
@@ -8,19 +8,19 @@ struct EventsView: View {
             List {
                 ForEach(0..<eventsVM.events.count, id: \.self) { index in
                     NavigationLink {
-                        EventForm(index: index)
+                        EventForm(title: "Edit", index: index)
                     } label: {
                         EventRow(event: eventsVM.events[index])
                     }
                 }
             }
-            .toolbar {
+            .toolbar { 
                 ToolbarItem(placement: .topBarTrailing) {
-                    Button(action: {
-                        eventsVM.appendEvent(event: Event(title: "Demo 123 ...", date: Date.now, textColor: .cyan))
-                    }, label: {
+                    NavigationLink {
+                        EventForm(title: "Add")
+                    } label: {
                         Label("Add", systemImage: "plus")
-                    })
+                    }
                 }
             }
             .navigationTitle("Events")
